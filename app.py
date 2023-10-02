e0a72577e148f3c5b4a1f6c72bbc52e528a97efd
Dan Scott <danscott2150@gmail.com>
2023-10-02 07:07:00 -0400

    refactor to newer ChatCompletion API

--- app.py
+++ app.py
@@ -13,32 +13,41 @@ app = Flask(__name__,
 )
 
 def get_colors(msg):
-    prompt = f"""
-    You are a color palette generating assistant that responds to text prompts for color palettes.
-    You should generate color palettes that fit the theme, mood, or instructions in the prompt.
-    The palettes should be between 2 and 6 colors.
 
-    Q: Convert the following verbal description of a color paleete into a list of colors: The Mediterranian Sea.
-    A: ["#006699", "#66CCCC", "#F0E68C", "#0088000", #"F08080"]
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a color palette generating assistant that responds to text prompts for color palettes. You should generate color palettes that fit the theme, mood, or instructions in the prompt. The palettes should be between 2 and 6 colors."
+        },
+        {
+            "role": "user",
+            "content": "Convert the following verbal description of a color paleete into a list of colors: The Mediterranian Sea."
+        },
+        {
+            "role": "assistant",
+            "content": '["#006699", "#66CCCC", "#F0E68C", "#0088000", #"F08080"]'
+        },
+        {
+            "role": "user",
+            "content": "Convert the following verbal description of a color paleete into a list of colors: sage, nature, earth"
+        },
+        {
+            "role": "assistant",
+            "content": '["#EDF1D6", "#9DC08B", "#609966", #"40513B"]'
+        },
+        {
+            "role": "user",
+            "content": f"Convert the following verbal description of a color paleete into a list of colors: {msg}"
+        },
+    ]
 
-    Q: Convert the following verbal description of a color paleete into a list of colors: sage, nature, earth
-    A: ["#EDF1D6", "#9DC08B", "#609966", #"40513B"]
-
-    Desired Format: a JSON array of hexadecimal color codes.
-
-    Text: Convert the following verbal description of a color paleete into a list of colors: {msg}
-    A:
-
-    Result:
-    """
-
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
+    response = openai.ChatCompletion.create(
+		messages=messages,
+        model="gpt-3.5-turbo",
         max_tokens=200,
     )
 
-    colors = json.loads(response["choices"][0]["text"])
+    colors = json.loads(response["choices"][0]["message"]["content"])
     return colors
 
 @app.route("/palette", methods=["POST"])
